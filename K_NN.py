625e268def2cd375a044d473e84db75869aa37af
reyhaneyazdani <reyhanehyx@gmail.com>
2023-06-30 19:35:42 +0330

    des

--- K_NN.py
+++ K_NN.py
@@ -46,45 +46,118 @@ from sklearn.neighbors import KNeighborsClassifier
 #     print()
 
 # ///////////////////////////////////////////////////////////////////////////////////////////
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-# Load the CSV file into a pandas DataFrame
-train_data = pd.read_csv("KNN/KNN_Features.csv",encoding='cp1252')
-test_data = pd.read_csv("KNN/TestData_dev1.csv")
+stop_words = set(
+        stopwords.words("english")
+        + [
+            "though",
+            "and",
+            "I",
+            "A",
+            "a",
+            "an",
+            "An",
+            "And",
+            "So",
+            ".",
+            ",",
+            ")",
+            "By",
+            "(",
+            "''",
+            "Other",
+            "The",
+            ";",
+            "however",
+            "still",
+            "the",
+            "They",
+            "For",
+            "for",
+            "also",
+            "In",
+            "This",
+            "When",
+            "It",
+            "so",
+            "Yes",
+            "yes",
+            "No",
+            "no",
+            "These",
+            "these",
+            "This",
+        ]
+    )
 
-# Preprocess the data by separating features and labels
-train_data = train_data.drop(["answer", "span", "question","syntatic_divergence","consistant_labels","span_POS_tags"], axis=1)
+stemmer = PorterStemmer()
 
-# Preprocess the data by separating features and labels
-X_train = train_data
-y_train = test_data["question"]
+questions_df = pd.read_csv('CSV-Files/QuestionsList.csv')
+contexts_df = pd.read_csv('CSV-Files/ContextList.csv')
 
-X_test = test_data.drop(["paragraphNo","titleNo"],axis=1)
-# Scale the features using StandardScaler
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+questions = questions_df['Question'].tolist()
+contexts = contexts_df['Context'].tolist()
 
-# Split the data into training and validation sets
-X_train_train, X_train_val, y_train_train, y_train_val = train_test_split(X_train_scaled, y_train, test_size=0.2, random_state=42)
+tokenized_contexts = []
+for context in contexts:
+    tokens = context.split()
+    tokens = [stemmer.stem(token) for token in tokens if token.lower() not in stop_words]
+    tokenized_contexts.append(tokens)
 
-# Train the KNN classifier using the training set
-knn = KNeighborsClassifier(n_neighbors=5, metric='euclidean')
-knn.fit(X_train_train, y_train_train)
+doc_freqs = {}
+for context in tokenized_contexts:
+    for term in set(context):
+        doc_freqs[term] = doc_freqs.get(term, 0) + 1
 
-# Evaluate the performance of the trained KNN model on the validation set
-val_preds = knn.predict(X_train_val)
-accuracy = (val_preds == y_train_val).mean()
-print("Validation accuracy:", accuracy)
+avg_doc_len = np.mean([len(context) for context in tokenized_contexts])
 
-# Use the trained KNN model to predict answers on the test set
-test_preds = []
-for i in range(len(X_test_scaled)):
-    neighbors = knn.kneighbors([X_test_scaled[i]], n_neighbors=5, return_distance=False)
-    majority_class = y_train[neighbors].mode()[0]
-    test_preds.append(majority_class)
+k1 = 1.2
+b = 0.75
 
-# Output the predicted answers to a new CSV file
-test_data["predicted_answer"] = test_preds
-test_data.to_csv("test_with_predictions.csv", index=False)
+N = len(tokenized_contexts)
+idf = {}
+for term, freq in doc_freqs.items():
+    idf[term] = np.log((N - freq + 0.5) / (freq + 0.5))
+
+doc_lens = [len(context) for context in tokenized_contexts]
+
+avg_doc_len = np.mean(doc_lens)
+
+bm25_scores = []
+for question in questions:
+    tokenized_question = question.split()
+    tokenized_question = [stemmer.stem(token) for token in tokenized_question if token.lower() not in stop_words]
+    scores = []
+    for i, context in enumerate(tokenized_contexts):
+        score = 0
+        for phrase in tokenized_question:
+            if " " in phrase:
+                phrase_words = phrase.split()
+                if all(word in context for word in phrase_words):
+                    idf_term = np.mean([idf.get(word, 0) for word in phrase_words])
+                    tf_term = context.count(phrase)
+                    doc_len = doc_lens[i]
+                    score += idf_term * ((tf_term * (k1 + 1)) / (tf_term + k1 * (1 - b + b * (doc_len / avg_doc_len))))
+            else:
+                if phrase in doc_freqs:
+                    idf_term = idf[phrase]
+                    tf_term = context.count(phrase)
+                    doc_len = doc_lens[i]
+                    score += idf_term * ((tf_term * (k1 + 1)) / (tf_term + k1 * (1 - b + b * (doc_len / avg_doc_len))))
+        scores.append(score)
+    bm25_scores.append(scores)
+
+top_contexts = []
+for scores in bm25_scores:
+    top_idxs = np.argsort(scores)[::-1][:3]
+    top_contexts.append([contexts[i] for i in top_idxs])
+
+answer_df = pd.DataFrame({'Question': questions, 'Answer': top_contexts})
+
+answer_df.to_csv('BM25_Ranking.csv', index=False)
+
